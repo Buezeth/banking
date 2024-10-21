53589c4d8d6d1fdf1c913bc6448635a898637fd3
Buezeth <chibuezeth@gmial.com>
2024-10-21 19:46:02 +0100

    Impliment all features on home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import { HeaderBox } from "@/components/HeaderBox";
+import { RightSidebar } from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "CT" };
+  const loggedIn = {
+    firstName: "CT",
+    lastName: "Devs",
+    email: "ctdevs@gmail.com",
+  };
 
   return (
     <section className="home">
@@ -22,7 +27,13 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
     </section>
   );
 };
